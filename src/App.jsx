716f037216fd0d28888b477daa72b78c8d3114c5
BravoClassic <gerald.akorli@gmail.com>
2023-10-21 00:55:23 -0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import { useEffect, useState } from "react";
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+import CoinInfo from "./components/CoinInfo";
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState(null);
+  const [search, setSearch] = useState("");
+  const [listLength, setListLength] = useState(0); 
+  const [PlatformType, setPlatformType] = useState("blockchain");
+  const [averageBlockTime, setAverageBlockTime] = useState(0); 
+  const [PlatformTypeList, setPlatformTypeList] = useState([]); 
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [commonAlgorithm, setCommonAlgorithm] = useState([]);
+  const searchItems = (searchValue) => {
+    console.log(list);
+    setSearch(searchValue);
+    if (search !== "") {
+      const filteredResults = Object.entries(list.Data).filter(([coin]) =>
+        coin.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setFilteredResults(filteredResults);
+    } else {
+      setFilteredResults([]);
+    }
+    console.log(filteredResults);
+  };
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const crypoList = await fetch(
+        `https://min-api.cryptocompare.com/data/all/coinlist?api_key=${API_KEY}`
+      );
+      const crypoListJson = await crypoList.json();
+      setList(crypoListJson);
+      let l = new Set(
+        Object.values(crypoListJson.Data).map((coin) =>
+          coin.PlatformType ? coin.PlatformType : null
+        )
+      );
+      l.delete(null);
+      
+      setAverageBlockTime(
+        Object.values(crypoListJson.Data).reduce((acc, coin) => {
+          return coin.PlatformType === PlatformType ? acc + coin.BlockTime : 0
+        }) / listLength
+      );
+      setPlatformTypeList(Array.from(l));
+      setListLength(Object.keys(crypoListJson.Data).length);
 
+    };
+
+    console.log(PlatformTypeList);
+    fetchAllCoinData().catch(console.error);
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="whole-page">
+        <h1>My Crypto List</h1>
+        <div>
+          <h2>Platform Type</h2>
+          {PlatformTypeList.map((platformType, key) =>
+            platformType ? (
+              <button key={key} onClick={() => setPlatformType(platformType)}>
+                {platformType}
+              </button>
+            ) : null
+          )}
+          <p>Total number of coins: {listLength}</p>
+          <p>Number of Platform Type coins: {PlatformTypeList.length}</p>
+          <p>
+            Average Block time of all {PlatformType} coins: {averageBlockTime || 0}
+          </p>
+        </div>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(inputString) => searchItems(inputString.target.value)}
+        />
+        <ul>
+          {search !== ""
+            ? filteredResults.map(([coin]) =>
+                list.Data[coin].PlatformType === PlatformType ? (
+                  <CoinInfo
+                    key={coin}
+                    image={list.Data[coin].ImageUrl}
+                    name={list.Data[coin].CoinName}
+                    symbol={list.Data[coin].Symbol}
+                  />
+                ) : null
+              )
+            : list &&
+              Object.entries(list.Data).map(([coin]) =>
+                list.Data[coin].PlatformType === PlatformType ? (
+                  <CoinInfo
+                    key={coin}
+                    image={list.Data[coin].ImageUrl}
+                    name={list.Data[coin].CoinName}
+                    symbol={list.Data[coin].Symbol}
+                  />
+                ) : null
+              )}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
